8dcd401de367be7d149ca70afdaa10f608852e04
MaelQllt <mael.quillat@orange.fr>
2026-08-24 10:46:04 +0200

    fix: bug csv

--- src/modules/RA/DataSource/components/DataSourceCSVFields.js
+++ src/modules/RA/DataSource/components/DataSourceCSVFields.js
@@ -22,7 +22,7 @@ import {
 import FieldGroup from '../../../../components/react-admin/FieldGroup';
 import FilePreview from './FilePreview';
 
-const DataSourceCSVFields = ({ translate: t, ...props }) => (
+const DataSourceCSVFields = ({ translate: t, type, ...props }) => (
   <FieldGroup {...props}>
     <FileInput
       source="file"
